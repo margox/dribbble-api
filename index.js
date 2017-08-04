38e52a86c4aa2f1f1889c15e3f897d66059f4051
Margox <margox@foxmail.com>
2017-08-04 13:13:36 +0800

    progress ++

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const http = require('http')
 const https = require('https')
 const url  = require('url')
 
+const access_token = '4455fa6b9d95ab096cc2fb1c12e5714731cd9119802b33555dcca1e3f2ed4a5b'
+
+const objToUrl = (obj) => {
+
+  var result = ''
+
+  Object.keys(obj).forEach(key => {
+    result += ('&' + key + '=' + obj[key])
+  })
+
+  return result
+
+}
+
 const server = http.createServer((req, res) => {
   let html = ''
   const { query } = url.parse(req.url, true)
@@ -38,7 +52,7 @@ const server = http.createServer((req, res) => {
             url: item.find('.dribbble-img img').attr('src'),
             attachments_count: newestShots[index].attachments_count || 0,
             likes_count: newestShots[index].likes_count || item.find('li.fav a').last().text(),
-            view_count: newestShots[index].view_count || item.find('li.views span').text(),
+            views_count: newestShots[index].view_count || item.find('li.views span').text(),
             comments_count: newestShots[index].comments_count || item.find('li.cmnt span').text(),
             user: {
               name: item.find('.attribution-user .url').text(),
@@ -49,8 +63,103 @@ const server = http.createServer((req, res) => {
         res.end(JSON.stringify(json))
       })
     })
+  } else if (query.type === 'shots') {
+    let json = ''
+    let param = Object.assign({}, query, { access_token })
+    delete param.type
+    param = objToUrl(param)
+    https.get({
+      hostname: 'margox.cn',
+      path: '/api/dribbble/shots?' + param,
+    }, (subres) => {
+      subres.on('data', (data) => {
+        json += data.toString()
+      })
+      subres.on('end', () => {
+        try {
+          let shots = JSON.parse(json)
+          if (shots instanceof Array) {
+            res.end(JSON.stringify(shots.map(item => {
+              const { id, name, username, avatar_url, pro } = item.user
+              item.user = { id, name, username, avatar_url, pro }
+              delete item.team
+              delete item.description
+              return item
+            })))
+          } else {
+            res.end('[]')
+          }
+        } catch (e) {
+          res.end('{"message": "Unknow Error."}')
+        }
+      })
+    })
+  } else if (query.type === 'shot') {
+    let json = ''
+    let param = { access_token }
+    param = objToUrl(param)
+    https.get({
+      hostname: 'margox.cn',
+      path: '/api/dribbble/shots/' + query.shot_id + '?' + param,
+    }, (subres) => {
+      subres.on('data', (data) => {
+        json += data.toString()
+      })
+      subres.on('end', () => {
+        try {
+          let shot = JSON.parse(json)
+          if (shot && shot.id) {
+            const { id, name, username, avatar_url, pro } = shot.user
+            shot.user = { id, name, username, avatar_url, pro }
+            delete shot.team
+            res.end(JSON.stringify(shot))
+          } else {
+            res.end('{}')
+          }
+        } catch (e) {
+          res.end('{"message": "Unknow Error."}')
+        }
+      })
+    })
+  } else if (query.type === 'shot_meta') {
+    let json = ''
+    let param = Object.assign({}, query, { access_token })
+    delete param.type
+    delete param.shot_id
+    delete param.meta_name
+    param = objToUrl(param)
+    https.get({
+      hostname: 'margox.cn',
+      path: '/api/dribbble/shots/' + query.shot_id + '/' + query.meta_name + '?' + param,
+    }, (subres) => {
+      subres.on('data', (data) => {
+        json += data.toString()
+      })
+      subres.on('end', () => {
+        try {
+          if (query.meta_name=== 'comments') {
+            let metas = JSON.parse(json)
+            if( metas instanceof Array) {
+              res.end(JSON.stringify(metas.map(item => {
+                const { id, name, username, avatar_url, pro } = item.user
+                item.user = { id, name, username, avatar_url, pro }
+                delete item.team
+                delete item.description
+                return item
+              })))
+            } else {
+              res.end('[]')
+            }
+          } else {
+            res.end(json)
+          }
+        } catch (e) {
+          res.end('{"message": "Unknow Error."}')
+        }
+      })
+    })
   } else {
-    res.end('{message: "Unknow Type."}')
+    res.end('{"message": "Unknow Type."}')
   }
 
 })
